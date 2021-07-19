16e4f0c24b73de9e6c27d4eade1f0a08f57ad7a6
Ali Sheikh <ali.sheikh@flexport.com>
2021-07-19 17:46:09 -0400

    fix

--- order_book.py
+++ order_book.py
@@ -9,49 +9,93 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 def process_order(order):
-    order_obj = Order( sender_pk=order['sender_pk'],receiver_pk=order['receiver_pk'], buy_currency=order['buy_currency'], sell_currency=order['sell_currency'], buy_amount=order['buy_amount'], sell_amount=order['sell_amount'] )
+    #Your code here
+
+    #Define Order Object
+    order_object = Order(sender_pk=order['sender_pk'], receiver_pk=order['receiver_pk'], buy_currency=order['buy_currency'], sell_currency=order['sell_currency'], buy_amount=order['buy_amount'], sell_amount=order['sell_amount'])
+
     if 'creator_id' in order.keys():
-        order_obj.creator_id = order['creator_id']
-    order = order_obj
+        #CASE when Creator Id exits
+        order_object.creator_id = order['creator_id']
+    order = order_object
+    #Add to Session
     session.add(order)
+    #Commit ot session
     session.commit()
 
-    orders = session.query(Order).filter(Order.filled == None).filter(Order.buy_currency == order.sell_currency).filter(Order.sell_currency == order.buy_currency).all()
+    orders = session.query(Order).filter(Order.buy_currency == order.sell_currency).filter(Order.sell_currency == order.buy_currency).filter(Order.filled == None).all()
+    market_rate = 0.0
     id = -1
-    rate = 0
 
-    for candidate in orders:
-        new_rate = candidate.sell_amount / candidate.buy_amount
-        if new_rate > rate:
-            rate = new_rate
-            id = candidate.id
+    for item in orders:
+        #check each item in orders
+        print(market_rate)
+        new_market_rate = item.sell_amount / item.buy_amount
+        print(new_market_rate)
 
-    if id != -1 and rate >= order.buy_amount / order.sell_amount:
-        other = session.query(Order).get(id)
+        if new_market_rate < market_rate:
+            continue
+        elif new_market_rate == market_rate:
+            market_rate = new_market_rate
+        elif new_market_rate > market_rate:
+            id = item.id
+            #Switch rate
+            market_rate = new_market_rate
+        else:
+            print("Check market rate")
+
+    if ((market_rate >= order.buy_amount / order.sell_amount) and (id != -1)):
+
+        market_fill_date = datetime.now()
+
+        partyOne = session.query(Order).get(id)
+        #Get order
         order = session.query(Order).get(order.id)
-        filled = datetime.now()
-        order.filled = filled
-        other.filled = filled
+
+        partyOne.filled = market_fill_date
+        order.filled = market_fill_date
+
         order.counterparty_id = id
-        other.counterparty_id = order.id
+        partyOne.counterparty_id = order.id
+
+        #Commit to Session
         session.commit()
-        new_order = {}
-        new_Order = None
-        if order.buy_amount > other.sell_amount:
-            new_order['sender_pk'] = order.sender_pk
-            new_order['receiver_pk'] = order.receiver_pk
-            new_order['buy_currency'] = order.buy_currency
-            new_order['sell_currency'] = order.sell_currency
-            new_order['buy_amount'] = order.buy_amount - other.sell_amount
-            new_order['sell_amount'] = order.sell_amount - other.buy_amount
-            new_order['creator_id'] = order.id
-            process_order(new_order)
-        elif other.buy_amount > order.sell_amount:
-            new_order['sender_pk'] = other.sender_pk
-            new_order['receiver_pk'] = other.receiver_pk
-            new_order['buy_currency'] = other.buy_currency
-            new_order['sell_currency'] = other.sell_currency
-            new_order['buy_amount'] = other.buy_amount - order.sell_amount
-            new_order['sell_amount'] = other.sell_amount - order.buy_amount
-            new_order['creator_id'] = other.id
-            process_order(new_order)
+
+        #define Transaction dictionary
+        transaction = {}
+
+        if partyOne.buy_amount > order.sell_amount:
+            #ID
+            transaction['creator_id'] = partyOne.id
+            #Sending
+            transaction['receiver_pk'] = partyOne.receiver_pk
+            transaction['sender_pk'] = partyOne.sender_pk
+            #Sell Currency
+            transaction['sell_currency'] = partyOne.sell_currency
+            transaction['buy_currency'] = partyOne.buy_currency
+            #Buy Amount
+            amount_bought = partyOne.buy_amount - order.sell_amount
+            amount_sold = partyOne.sell_amount - order.buy_amount
+            transaction['buy_amount'] = amount_bought
+            transaction['sell_amount'] = amount_sold
+            ##PROCESS TRANSACTIOn
+            process_order(transaction)
+
+        elif order.buy_amount > partyOne.sell_amount:
+            #ID
+            transaction['creator_id'] = order.id
+            #Sending
+            transaction['receiver_pk'] = order.receiver_pk
+            transaction['sender_pk'] = order.sender_pk
+
+            transaction['buy_currency'] = order.buy_currency
+            transaction['sell_currency'] = order.sell_currency
+
+            #Buy Amount
+            s_amount_bought = order.buy_amount - partyOne.sell_amount
+            s_amount_sold = order.sell_amount - partyOne.buy_amount
+            #Transaction
+            transaction['buy_amount'] = s_amount_bought
+            transaction['sell_amount'] = s_amount_sold
+            #Process
+            process_order(transaction)
